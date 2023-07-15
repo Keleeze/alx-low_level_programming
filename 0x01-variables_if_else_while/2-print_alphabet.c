179483b97aa599c17dc70eadffc62a03a8c9ca30
Kelechukwu <kelechukwuezema@.com>
2023-07-14 21:06:30 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Main - Entry point
- * Descripton: 'Print alphabets lowercase'
- * Return: Always 0
+ *main - Entry point
+ *Descripton: 'Print alphabets lowercase'
+ *Return: Always 0
  */
 int main(void)
 {
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 /**
- * Main - Entry point
- * Descripton: 'Print alphabets lowercase'
- * Return: Always 0
+ *main - Entry point
+ *Descripton: 'Print alphabets lowercase and uppercase'
+ *Return: always 0
  */
 int main(void)
 {
 	int n = 97;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
 	}
 	putchar('\n');
 	return (0);
