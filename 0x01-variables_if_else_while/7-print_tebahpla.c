eca7551a8298f9d5a51862ac595ca7f6026fe289
Kelechukwu <kelechukwuezema@.com>
2023-07-15 02:42:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+/**
+ *main - Entry point
+ *Description: 'Check for positive ngative or zero'
+ *Return: always 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n)
+		n--;
+	}
+	putchar('\n');
+	}
+	return (0);
+}
