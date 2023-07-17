54a0b73aca50c6a7a362786079cbdf539adae512
Kelechukwu <kelechukwuezema@.com>
2023-07-17 08:26:46 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 /**
 *main - Entry point
-*Description: 'print all possible combination  of numbers three digits'
+*Description: print all possible combination  of numbers three digits
 *Return: always 0
 */
 int main(void)
 {
-	int n = '0'
-	int m = '0'
-	int l = '0'
-	
-	for (l = '0'; l <= '9'; l++)
+	int l = '0';
+	int m = '0';
+	int n = '0';
+
+	for (n = '0'; n <= '9'; n++)
 	{
 		for (m = '0'; m <= '9'; m++)
 		{
-			for (n = '0'; l <= '9'; l++)
+			for (l = '0'; l <= '9'; l++)
 			{
-				if (!((n == m) || ( m == l) || (m > n)|| (l  > m)));
+				if (!((l == m) || (m == n) || (m > l) ||(n > m)));
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if(! (n == '9' &&  l == '7' && m == '8'))
+					if (!(l == '9' && n == '7' && m == '8'))
 					{
 						putchar(',');
 						putchar(' ');
