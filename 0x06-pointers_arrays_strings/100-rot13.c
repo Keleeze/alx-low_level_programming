9709c458a6b8a7d52583e0aea8c05fb62c2d3e8f
Kelechukwu <kelechukwuezema@.com>
2023-07-27 03:44:11 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,7 +5,7 @@
  * rot13 - encoder rot13
  * @s: pointer to string params
  *
- * Return *s
+ * Return: *s
  */
 
 char *rot13(char *s)
@@ -14,7 +14,7 @@ char *rot13(char *s)
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
