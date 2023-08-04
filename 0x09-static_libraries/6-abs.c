06594b538025b97f9afbbb001c3c1343544b8a70
Kelechukwu <kelechukwuezema@.com>
2023-08-04 07:03:14 -0700

    static libary

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  _abs - compares the absolute value of an integer
+ *  @n: the int tho check
+ *  Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
